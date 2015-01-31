a1d17aff5e54721475366cbab28cdd2a4d0ade15
Fabio Berger <fabioberger1991@gmail.com>
2015-01-30 20:39:19 -0500

    Now grabbing setup from file

--- controllers/users.go
+++ controllers/users.go
@@ -79,7 +79,7 @@ func (u Users) Create(res http.ResponseWriter, req *http.Request) {
 		panic(err)
 	}
 
-	// Create and save the user
+	// Create and save user
 	user := &models.User{
 		Name:           name,
 		Email:          email,
